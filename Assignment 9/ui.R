95734b54458c01205af52a233b787da14ad692c5
sheHulk <rsmyrer@gmail.com>
2019-04-15 19:05:21 -0600

    length and width updates to min and max

--- Assignment 9/ui.R
+++ Assignment 9/ui.R
@@ -12,28 +12,36 @@ dashboardPage(
     sliderInput(
       "SLength",
       "Sepal Length:",
-      min = 0, max = 50, value = 1
+      min = min(iris$Sepal.Length), 
+      max = max(iris$Sepal.Length),
+      value = min(iris$Sepal.Length)
     ),
 
 #Sepal Width
     sliderInput(
       "SWidth",
       "Sepal Width:",
-      min = 0, max = 50, value = 1
+      min = min(iris$Sepal.Width),
+      max = max(iris$Sepal.Width),
+      value = min(iris$Sepal.Width)
     ),
 
 #Petal Legth
     sliderInput(
           "Petal Length",
           "Petal Length:",
-          min = 0, max = 50, value = 1
+          min = min(iris$Petal.Length),
+          max = max(iris$Petal.Length),
+          value = min(iris$Petal.Length)
     ), 
   
 #Petal Width
     sliderInput(
       "Petal Width",
       "Petal Width:",
-      min = 0, max = 50, value = 1
+      min = min(iris$Petal.Width),
+      max = max(iris$Petal.Width),
+      value = min(iris$Petal.Width)
     )
 
   ),
