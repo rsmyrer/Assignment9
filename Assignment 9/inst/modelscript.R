d0d8cbc74331960d7857631c4a8e3e289ec062f0
sheHulk <rsmyrer@gmail.com>
2019-04-15 19:21:24 -0600

    iris classifier model

--- Assignment 9/inst/modelscript.R
+++ Assignment 9/inst/modelscript.R
@@ -1,1 +1,30 @@
 #model script
+
+IrisNames <- c("Setosa", "Versicolor", "Viginica")
+
+y = as.numeric(iris$Species) - 1
+
+x <- select(iris, -Species)
+x <- as.matrix(x)
+
+params <- list(
+  "objective" = "multi:softprob",
+  "eval_metric" = "mlogloss",
+  "num_class" = length(table(y)),
+  "eta" = .05,
+  "max_depth" = 3
+)
+
+cv.nround = 100
+bst.cv <- xgb.cv(params = params, data = x, label = y, nfold = 5, 
+                 missing = NA, prediction = TRUE, nrounds = cv.nround)
+
+
+nrounds <- which (bst.cv$evaluation_log$test_mlogloss_mean == min(bst.cv$evaluation_log$test_mlogloss_mean))
+
+IrisClassifier <- xgboost(params = params, 
+                          data = x, 
+                          label = y,
+                          missing = NA,
+                          nrounds = nrounds)
+
